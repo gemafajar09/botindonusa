5ededce9486885312c836ec7e32b81f7408d7e8c
gemafajar09 <gemafajar09@gmail.com>
2023-03-15 11:28:49 +0700

    new

--- bot.js
+++ bot.js
@@ -221,12 +221,6 @@ var checkMe = ['ADMINISTRATOR','CREATE_INSTANT_INVITE','KICK_MEMBERS','BAN_MEMBE
                 .setURL(WEBSITE_URL)
                 .setStyle('LINK')
             )
-            // .addComponents(
-            //   new MessageButton()
-            //     .setLabel('Connect')
-            //     .setURL(CONNECT_URL)
-            //     .setStyle('LINK')
-            // )
             .addComponents(
               new MessageButton()
                 .setLabel('Cfx Status')
@@ -258,7 +252,7 @@ var checkMe = ['ADMINISTRATOR','CREATE_INSTANT_INVITE','KICK_MEMBERS','BAN_MEMBE
       getPlayers().then((players) => {
     bot.user.setPresence({
       activities: [{
-          name: `${SERVER_NAME}`,
+          name: `Palying ${GetNumPlayerIndices()} player(s)`,
           type: "WATCHING"
       }], status: "online"
     })
